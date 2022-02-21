4ada9e83e7e5552ab89366f275d407aab5d75428
mbKirby <matthew.kirby90@yahoo.com>
2022-02-21 16:19:42 -0500

    pipfile

--- personalWebsite/settings.py
+++ personalWebsite/settings.py
@@ -142,4 +142,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
-django_heroku.settings(locals)
+django_heroku.settings(locals())
